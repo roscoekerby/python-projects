01701f66bda34f7da5ebe96016eacf625b4cdd81
Roscoe Kerby <56516234+roscoekerby@users.noreply.github.com>
2022-10-03 16:34:23 +0200

    Add files via upload

---
+++ helloworld/helloworld.py
@@ -0,0 +1,12 @@
+# Template code adapted from PyCharm default main python file
+# My version of hello world - Hi {user}!
+def print_hi(name_to_print):
+    print(f'Hi, {name_to_print}!')
+
+
+if __name__ == '__main__':
+    print("Please type your name :)")
+    input_name = input()
+    print_hi(input_name)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
