2e8903c61ff1f280024b80ab42f626c1585249ea
roscoe.kerby <roscoe.kerby@gmail.com>
2022-10-19 18:46:32 +0200

    haha I fixed Webb

--- traceroute/webb_trace.py
+++ traceroute/webb_trace.py
@@ -1,5 +1,8 @@
-from webb import webb
+import sys
+# insert at position 1 in the path, as 0 is the path of this file.
+sys.path.insert(1, './')
+import webb_fixed
 
-webb.traceroute("www.google.com")
-webb.traceroute("www.google.com", 'trace.txt')
-print(webb.get_ip("www.google.com"))
+webb_fixed.traceroute("www.google.com")
+webb_fixed.traceroute("www.google.com", 'trace.txt')
+print(webb_fixed.get_ip("www.google.com"))
